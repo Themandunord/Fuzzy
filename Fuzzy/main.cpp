9c3d31810e57ebbd6e679c8faa2c74a7f7f5f9cd
Lespagnol <plespagnol@sfr.fr>
2014-03-18 14:45:03 +0100

    giti

--- Fuzzy/main.cpp
+++ Fuzzy/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "core/valuemodel.h"
 #include "fuzzy/notminus1.h"
+#include "core/unaryexpressionmodel.h"
 
 using namespace std;
 
@@ -16,6 +17,9 @@ int main()
     /* Test de NotMinus1 */
     fuzzy::NotMinus1<float>* nm1 = new fuzzy::NotMinus1<float>();
     cout << nm1->evaluate(vm) << endl;
+
+    /* Test UnaryExpression avec NotMinus1 */
+    core::UnaryExpressionModel<float>* uem = new core::UnaryExpressionModel<float>(&vm, &nm1);
 
     return 0;
 }
