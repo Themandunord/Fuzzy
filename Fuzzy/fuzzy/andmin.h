7d02bb3df6ec5a41064e05956a3235369edba20f
Antique Dernnell <a.dernnell@gmail.com>
2014-03-18 15:42:23 +0100

    and

---
+++ Fuzzy/fuzzy/andmin.h
@@ -0,0 +1,33 @@
+#ifndef ANDMIN_H
+#define ANDMIN_H
+
+#include "and.h"
+
+namespace fuzzy{
+    template<class T>
+    class AndMin : And<T>{
+    public:
+        AndMin();
+        virtual ~AndMin();
+        virtual T evaluate(core::Expression<T>* l,core::Expression<T>* r) const;
+    };
+
+    template<class T>
+    AndMin<T>::AndMin(){
+
+    }
+
+    template<class T>
+    AndMin<T>::~AndMin(){
+
+    }
+
+    template<class T>
+    T AndMin<T>::evaluate(core::Expression<T>* l,core::Expression<T>* r){
+        T vl = l->evaluate();
+        T vr = r->evaluate();
+        return (vl<vr)?vl:vl;
+    }
+}
+
+#endif // ANDMIN_H
