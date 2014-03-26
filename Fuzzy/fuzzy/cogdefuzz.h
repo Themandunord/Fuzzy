64fb9cdbcc04f7cf437cc42b6d74cfde6b6dd1bc
Remy <plespagnol@sfr.fr>
2014-03-26 19:18:29 +0100

    Impl CogDefuzz

--- Fuzzy/fuzzy/cogdefuzz.h
+++ Fuzzy/fuzzy/cogdefuzz.h
@@ -27,8 +27,21 @@ namespace fuzzy{
     }
 
     template<class T>
-    T Codefuzz<T>::defuzz(std::pair<std::vector<T>*,std::vector<T>* >*) const{
+    T CogDefuzz<T>::defuzz(std::pair<std::vector<T>*,std::vector<T>* >* shape) const{
+        T _num;
+        T _den;
 
+        typename std::vector<T>::const_iterator it_first = shape->first->begin();
+        typename std::vector<T>::const_iterator it_second = shape->second->begin();
+
+        for(int i = 0; it_first != shape->first->end(); i++){
+            _num += *it_first + *it_second;
+            _den += *it_second;
+            it_first++;
+            it_second++;
+        }
+
+        return _num / _den;
     }
 }
 
