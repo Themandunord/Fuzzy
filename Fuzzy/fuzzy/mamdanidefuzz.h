9b38fa4f44b77582c05e52ae6c2f1fda799e56bc
Remy <plespagnol@sfr.fr>
2014-03-26 15:54:40 +0100

    commit

--- Fuzzy/fuzzy/mamdanidefuzz.h
+++ Fuzzy/fuzzy/mamdanidefuzz.h
@@ -2,13 +2,15 @@
 #define MAMDANIDEFUZZ_H
 
 #include "../core/binaryexpression.h"
+#include <iostream>
+#include <vector>
 
 namespace fuzzy {
     template<class T>
     class MamdaniDefuzz : public core::BinaryExpression<T>{
     public:
-        virtual T evaluate(core::Expression<T>* l, core::Expession<T>* r) const = 0;
-
+        virtual T evaluate(core::Expression<T>* l, core::Expession<T>* r) const;
+        virtual T defuzz(std::pair<std::vector<T>*,std::vector<T>* >*) const = 0;
     };
 
 }
