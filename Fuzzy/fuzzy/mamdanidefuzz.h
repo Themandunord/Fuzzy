ea7d830a9bc234c2186c4e765214d408abdc0cdd
Remy <plespagnol@sfr.fr>
2014-03-26 19:11:54 +0100

    mam

--- Fuzzy/fuzzy/mamdanidefuzz.h
+++ Fuzzy/fuzzy/mamdanidefuzz.h
@@ -10,20 +10,14 @@ namespace fuzzy {
     template<class T>
     class MamdaniDefuzz : public core::BinaryExpression<T>{
     public:
-<<<<<<< HEAD
-        virtual T evaluate(core::Expression<T>* l, core::Expression<T>* r) const;
-=======
         MamdaniDefuzz(const T& _min, const T& _max, const T& _step);
         virtual ~MamdaniDefuzz(){}
         virtual T evaluate(core::Expression<T>* l, core::Expession<T>* r) const;
->>>>>>> de92fe64f3e4f5cd6dda141888f2d99db54c9a60
         virtual T defuzz(std::pair<std::vector<T>*,std::vector<T>* >*) const = 0;
     private:
         T m_min, m_max, m_step;
     };
 
-<<<<<<< HEAD
-=======
     template<class T>
     MamdaniDefuzz<T>::MamdaniDefuzz(const T& _min, const T& _max, const T& _step)
         : m_min(_min), m_max(_max), m_step(_step)
@@ -35,7 +29,6 @@ namespace fuzzy {
     MamdaniDefuzz<T>::evaluate(core::Expression<T>* l, core::Expession<T>* r){
         return defuzz(core::ShapeView<T>::getInstance().setParam(l,r,m_min,m_max,m_step).getShape());
     }
->>>>>>> de92fe64f3e4f5cd6dda141888f2d99db54c9a60
 
 
 }
