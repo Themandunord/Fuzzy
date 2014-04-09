f8fe41060e52abba40a892d23a0af2bbceb0f49b
Antique Dernnell <a.dernnell@gmail.com>
2014-04-09 09:00:28 +0200

    naryexpression

--- Fuzzy/core/NaryExpression.h
+++ Fuzzy/core/NaryExpression.h
@@ -1,10 +1,16 @@
 #ifndef NARYEXPRESSION_H
 #define NARYEXPRESSION_H
 
+#include <vector>
+
 namespace core
 {
+    template<class T>
     class NaryExpression : public Expression
     {
+    public:
+        virtual ~NaryExpression();
+        virtual T evaluate(std::vector<Expression<T>*> operands) const = 0;
 
     };
 }
